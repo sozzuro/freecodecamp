45e920f3008ca3bacfdc1dee26cbea02a63838cb
Denis Radivil <sozzuro@gmail.com>
2022-01-30 23:28:23 +0300

    feat: completed Manipulating Complex Objects task

---
+++ 40.js
@@ -0,0 +1,28 @@
+/*
+Manipulating Complex Objects
+
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+*/
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "qqqqqq",
+      "title": "wwwwww",
+      "release_year": 1991,
+      "formats": [
+        "mp3",
+        "flac"
+      ]
+    },
+  ];
