6e56b249d90d8b4d9f5442c04d86fe127cbe53eb
Denis Radivil <sozzuro@gmail.com>
2022-01-30 20:19:37 +0300

    feat: completed Understanding Boolean Values task

---
+++ 6.js
@@ -0,0 +1,14 @@
+/*
+Understanding Boolean Values
+Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+*/
+
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
+  
+  welcomeToBooleans();
