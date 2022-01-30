65c2323dad9c5e82acf3b1c85a6af00846440d08
Denis Radivil <sozzuro@gmail.com>
2022-01-30 22:49:57 +0300

    feat: completed Returning Boolean Values from Functions task

---
+++ 28.js
@@ -0,0 +1,12 @@
+/*
+Returning Boolean Values from Functions
+Fix the function isLess to remove the if/else statements.
+*/
+
+function isLess(a, b) {
+    // Only change code below this line
+      return a <= b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
