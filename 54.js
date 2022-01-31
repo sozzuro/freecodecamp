47cd5436dd9927f56bdda6493b9cb7f5628d8fa1
Denis Radivil <sozzuro@gmail.com>
2022-01-31 13:00:56 +0300

    feat: completed Generate Random Whole Numbers with JavaScript task

---
+++ 54.js
@@ -0,0 +1,12 @@
+/*
+Generate Random Whole Numbers with JavaScript
+
+Use this technique to generate and return a random whole number between 0 and 9.
+*/
+
+function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random() * 10);
+  }
